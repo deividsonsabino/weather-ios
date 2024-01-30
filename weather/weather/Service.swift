ec6fa98ec6ed458cfde8adb6fa69d1cd74bac1c2
Deividson Sabino <xdeividsabinox@live.com>
2024-01-29 21:46:14 -0300

    feat :sparkles: : add json decoder

--- weather/weather/Service.swift
+++ weather/weather/Service.swift
@@ -28,3 +28,58 @@ class Service {
         task.resume()
     }
 }
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let forecastResponse = try? JSONDecoder().decode(ForecastResponse.self, from: jsonData)
+
+import Foundation
+
+// MARK: - ForecastResponse
+struct ForecastResponse: Codable {
+    let current: Forecast
+    let hourly: [Forecast]
+    let daily: [DailyForecast]
+}
+
+
+// MARK: - Forecast
+struct Forecast: Codable {
+    let dt: Int
+    let temp: Double
+    let humidity: Int
+    let windSpeed: Double
+    let weather: [Weather]
+
+    enum CodingKeys: String, CodingKey {
+        case dt, temp, humidity
+        case windSpeed = "wind_speed"
+        case weather
+    }
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+// MARK: - DailyForecast
+struct DailyForecast: Codable {
+    let dt : Int
+    let temp: Temp
+    let weather: [Weather]
+}
+
+// MARK: - Temp
+struct Temp: Codable {
+    let day, min, max, night: Double
+    let eve, morn: Double
+}
+
+// MARK: - Minutely
+struct Minutely: Codable {
+    let dt, precipitation: Int
+}
+
